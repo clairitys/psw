2354b1af545e38da3fbc194c5d07474cc0f8f575
ana <anamorais2507@gmail.com>
2026-06-08 12:27:48 -0300

    Delete projeto/frontend/src/pages directory

--- projeto/frontend/src/pages/Lista.jsx
+++
@@ -1,8 +0,0 @@
-export function Listas() {
-  return (
-    <div style={{ color: 'white', textAlign: 'center', marginTop: '100px' }}>
-      <h1>Minhas Listas 📋</h1>
-      <p>Em construção...</p>
-    </div>
-  );
-}
