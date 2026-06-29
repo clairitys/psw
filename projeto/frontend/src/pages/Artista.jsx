558b2df2a17a28cf109ffe081970a81ac0ddabd7
manu <rrodriguess.manuela@gmail.com>
2026-06-29 23:48:04 +0000

    fotos

--- projeto/frontend/src/pages/Artista.jsx
+++ projeto/frontend/src/pages/Artista.jsx
@@ -56,7 +56,7 @@ export function Artista() {
             <div className="artist-header">
               <div className="artist-image-wrapper">
                 <img
-                  src={formatAvatarUrl(artistaDetalhe.foto, '/img/user.jpg')}
+                  src={formatAvatarUrl(artistaDetalhe.foto)}
                   alt={artistaDetalhe.nome}
                   className="artist-detail-image"
                 />
